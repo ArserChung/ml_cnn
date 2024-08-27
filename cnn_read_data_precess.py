d2e272d2e37dd2300af7a80728931832443eec3b
ArserChung <xzhong052@icloud.com>
2024-08-27 23:20:46 +0800

    2024/8/27 11.20pm traing model

--- cnn_read_data_precess.py
+++ cnn_read_data_precess.py
@@ -1,6 +1,6 @@
 import numpy as np
 from cnn_basic import create_cnn
-from keras.models import Sequential
+
 #loading npy
 train_feature = np.load('train_feature.npy')
 train_label = np.load('train_label.npy')
@@ -24,6 +24,34 @@ test_feature_normal = test_feature/255
 train_label_onehot = np.eye(2)[train_label]
 test_label_onehot = np.eye(2)[test_label]
 
-model = Sequential()
-
 model = create_cnn()
+try:
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy']
+    )
+    print("model compile.....")
+except:
+    print("compile fail,check in cnn_baisc.py")
+    pass
+else :
+    print("sucess compile model")
+
+model.fit(
+    x=train_feature_normal,
+    y=train_label_onehot,
+    validation_split=0.2,
+    epochs=10,
+    batch_size=200,
+    verbose=2
+)
+try:
+    model.save('cnn_recognize_pet.h5')
+except:
+    print("save the model fail")
+else:
+    _,score = model.evaluate(test_feature_normal,test_label_onehot)
+    print("accuracy="+str(score))
+    print("model saving sucess")
+    del model
